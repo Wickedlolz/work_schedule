d8aa7da6790654a7a6a28eb07445315f222a5be8
Viktor Dimitrov <viktor.dimitrov.dev@gmail.com>
2025-10-24 20:09:05 +0300

    feat: add Toaster component for improved user notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import SchedulePage from "./components/Schedule";
+import { Toaster } from "./components/ui/sonner";
 
 const App = () => {
   return (
     <main className="w-full max-w-screen overflow-x-auto px-2 sm:px-4">
       <SchedulePage />
+      <Toaster />
     </main>
   );
 };
